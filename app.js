00aa35217ea5608654a9648c06acd5d5f316a20a
ccyang02 <chiachingyang02@gmail.com>
2020-11-26 00:09:07 +0800

    feat: signup form validation & flash messages

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const exhbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
 app.engine('handlebars', exhbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
