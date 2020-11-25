0eec5ac21f3c02f346510a503afb6bae0e44bc91
ccyang02 <chiachingyang02@gmail.com>
2020-11-26 00:01:22 +0800

    feat: build user model & user register feature

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const exhbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.engine('handlebars', exhbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
