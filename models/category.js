bb0183f4f2662aa9bb04449dce70b4f8e32938b2
ccyang02 <chiachingyang02@gmail.com>
2020-12-01 22:31:05 +0800

    feat: add category seed data

--- models/category.js
+++ models/category.js
@@ -1,18 +1,18 @@
-'use strict';
+'use strict'
 const {
   Model
-} = require('sequelize');
+} = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Category extends Model {
     static associate(models) {
       Category.hasMany(models.Restaurant)
     }
-  };
+  }
   Category.init({
     name: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'Category',
-  });
+  })
   return Category;
-};
+}
