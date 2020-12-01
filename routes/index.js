e0297bd022c175c3f70e84929e800ae1bb644b9d
ccyang02 <chiachingyang02@gmail.com>
2020-12-01 23:54:50 +0800

    feat: add admin categories delete feature

--- routes/index.js
+++ routes/index.js
@@ -41,6 +41,7 @@ module.exports = (app, passport) => {
 
   app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
   app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
+  app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
   app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
   app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
 
